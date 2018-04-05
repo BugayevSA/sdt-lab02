ef209265d960101c522083b3dafee146f587c99d
Bugaev Sergey <BugayevSA@mpei.ru>
2018-04-05 15:09:35 +0300

    eleventh

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
-//программа является учебной
+ #include <iostream>
+
  using namespace std;
 
  int main()
  {
  int a, b;
  cout << "Enter A and B: ";
-     cin >> a >> b;
-     cout << "A + B = " << a + b << '\n';
-     cout << "A*2 =" << a*2 << '\n';
-     cout << "A - B = " << a - b << '\n';
+ cin >> a >> b;
+ cout << "A + B = " << a + b << '\n'
+ << "A * 2 = " << a / 2 << '\n'
+ << "A - B = " << a - b << '\n'
+ << "A * B = " << a / b << '\n';
  }
